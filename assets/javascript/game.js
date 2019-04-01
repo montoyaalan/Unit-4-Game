d1e68a5291dd3dd7afc78d09c78b4b0d022d9420
Alan Montoya <alanmontoya2010@att.net>
2019-03-31 22:51:02 -0500

    variables have been declared with random formula from example

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,3 +7,19 @@ $( document ).ready(function(){
     //THE NUMBER TO BE REACHED MUST BE BETWEEN 19 AND 120
     // THIS IS WHERE MOE HELPED ME FIGURE OUT THE CODE BUT I COULDNT REMEMBER WHY HE SAID WE MUST ADD 1 
     var Random=Math.floor(Math.random()*101+19)
+    
+//APPENDING
+//DISPLAYING "APPENDING" RANDOM NUMBER TO THE randNum ID IN HTML FILE 
+  $('#randNumb').text(Random);
+
+
+// CREATING VARIABLES AND RANDOM NUMBERS BETWEEN 1-12 FOR THE CRYSTALS 
+var green= Math.floor(Math.random()*11+1)
+var yellow= Math.floor(Math.random()*11+1)
+var red= Math.floor(Math.random()*11+1)
+var blue= Math.floor(Math.random()*11+1)
+
+//THESE ARE THE VARIABLES FOR MY COUNTERS
+var totalScore= 0; 
+var winsCounter= 0;
+var lossesCounter= 0;
