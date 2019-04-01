84d43221cff063581818e95239266f455e9158c8
Alan Montoya <alanmontoya2010@att.net>
2019-03-31 23:10:28 -0500

    created functions for my counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,14 +10,14 @@ $( document ).ready(function(){
     
 //APPENDING
 //DISPLAYING "APPENDING" RANDOM NUMBER TO THE randNum ID IN HTML FILE 
-  $('#randNumb').text(Random);
+$('#randNumb').text(Random);
 
 
-// CREATING VARIABLES AND RANDOM NUMBERS BETWEEN 1-12 FOR THE CRYSTALS 
-var green= Math.floor(Math.random()*11+1)
-var yellow= Math.floor(Math.random()*11+1)
-var red= Math.floor(Math.random()*11+1)
-var blue= Math.floor(Math.random()*11+1)
+// CREATING VARIABLES AND RANDOM NUMBERS BETWEEN 1-12 FOR EACH CRYSTAL
+green = Math.floor(Math.random()*11+1)
+yellow = Math.floor(Math.random()*11+1)
+red = Math.floor(Math.random()*11+1)
+blue = Math.floor(Math.random()*11+1)
 
 //THESE ARE THE VARIABLES FOR MY COUNTERS
 var totalScore= 0; 
@@ -25,5 +25,39 @@ var winsCounter= 0;
 var lossesCounter= 0;
 
 //USING JQUERY TO UPDATE HTML FILE VIA DOM
-$('#numberWins').text(winsCounter);
-$('#numberLosses').text(lossesCounter);
+$('#numbWins').text(winsCounter);
+$('#numbLosses').text(lossesCounter);
+
+
+// FUNCTIONS GO IN BELOW
+//NEED TO ADD FUNCTION TO RESET THE GAME 
+//LOOK AT HANGMAN GAME FOR FUNCTION INSPIRATION 
+
+function reset(){
+    Random=Math.floor(Math.random()*101+19);
+    console.log(Random)
+    $('#randNumb').text(Random);
+    green= Math.floor(Math.random()*11+1);
+    yellow= Math.floor(Math.random()*11+1);
+    red= Math.floor(Math.random()*11+1);
+    blue= Math.floor(Math.random()*11+1);
+    totalScore= 0;
+    $('#playerScores').text(totalScore);
+    } 
+
+//ADDS WINS TO THE TOTAL SCORE 
+function winner (){
+    alert("You won!");
+  winsCounter++; 
+  $('#numbWins').text(winsCounter);
+  reset();
+}
+
+//ADDS LOSSES TO THE TOTAL SCORE 
+function loser (){
+    alert ("You lose!");
+      lossesCounter++;
+      $('#numbLosses').text(lossesCounter);
+      reset()
+    }
+    
